9ad83a39ea7d5982ab43370f579fbb648c50321c
Mat <mathew.b.lee@gmail.com>
2020-06-14 17:08:37 -0600

    Structure project with setup.py and Makefile

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+# this line is so that our README can be used as part of our package
+with open('README.rst', encoding='UTF-8') as f:
+    readme = f.read()
+
+setup(
+    name='pgbackup',
+    version='0.1.0',
+    description='Databse backups locally or to AWS S3',
+    long_description=readme,
+    author='Mat',
+    author_email='mat@example.com',
+    install_requires=[],
+    packages=find_packages('src'),
+    package_dir={'': 'src'}
+)
